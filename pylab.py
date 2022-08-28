7089d32b1332ca67419ddf44ac386966d962b411
vinicius <vnszero@gmail.com>
2022-08-27 21:18:03 -0300

    update Axis, Polynomial and Tf

--- pylab.py
+++ pylab.py
@@ -1,33 +1,96 @@
-def tf():
-	pass
-
-def plot():
-	pass
-
 class Axis():
-	def __init__(begin, step, end):
+	'''
+		defines an axis array between [begin, end] by step
+		
+		e.g.
+		x = Axis(1, 0.1, 1.2)
+		print(x.getAxis()) 
+			out: [1, 1.1, 1,2]
+
+		y = Axis(4, -1, 0)
+		print(x.getAxis())
+			out: [4, 3, 2, 1, 0]
+
+		y = Axis(10, 3, 10)
+		print(x.getAxis())
+			out: [10]
+
+		y = Axis(1, -1, 10)
+		print(x.getAxis())
+			out: []
+	'''
+	def __init__(self, begin, step, end):
 		self.begin = begin
 		self.step = step
 		self.end = end
-		self.axis = [begin:step:end]
+		self.axis = []
 
-	def __str__() -> str:
-		return f'{} to {} by {}'.format(self.begin, self.end, self.step)
+		# range function doesn't work for float numbers
+		x = begin
+		if step > 0 and begin < end:
+			while(x < end):
+				self.axis.append(x)
+				x += step
+		elif step < 0 and begin > end:
+			while(x > end):
+				self.axis.append(x)
+				x += step
+		elif begin == end:
+			self.axis.append(begin)
+		else:
+			self.axis = []
 
-class polynomial():
-	def __init__(coefficients:List, var:chr):
+	def __str__(self) -> str:
+		return '{} to {} by step {}'.format(self.begin, self.end, self.step)
+
+	def getAxis(self) -> list:
+		return self.axis
+
+class Polynomial():
+	'''
+		Defines a polynomial expression based on coefficients passed
+		
+		e.g.
+		f = Polynomial([3,2,1], 'x')
+		means: f(x) = 3x^2 + 2x + 1
+
+		f = Polynomial([3,0,1], 't')
+		means: f(t) = 3t^2 + 1
+	'''
+	def __init__(self, coefficients:list, var:chr):
 		self.coefficients = coefficients
 		self.degree = len(coefficients)-1
 		self.var = var
 
-	def __str__():
-		#warning: troubles to represent poly with 0 as coeff
+	def __str__(self):
+		# The final expression starts empty
 		out = ''
+		# Highest degree at beginning
 		degree = self.degree
 		for coefficient in self.coefficients:
-			out += f'{}*{}^{} '.format(coefficient,var,degree)
+			if coefficient != 0 and degree != 0:
+				# To represent a*x^b
+				out += '{}*{}^{} + '.format(coefficient, self.var, degree)
+			elif coefficient != 0 and degree == 0:
+				# To represent independent term c
+				out += '{}'.format(coefficient)
 			degree -= 1
 		return out
+	
+	def getCoeff(self):
+		return self.coefficients
 
-class Freqs():
-	def __init__(numerator, denominator, time:Axis)
+class Tf():
+	'''
+		Defines a transfer function based in two arrays and a varible name char
+		's' is defined by the default value for var
+	'''
+	def __init__(self, numerator_coeff:list, denominator_coeff:list, var='s'):
+		self.numerator = Polynomial(numerator_coeff, var)
+		self.denominator = Polynomial(denominator_coeff, var)
+		self.var = var
+	
+	def __str__(self):
+		out = '( {} ) / '.format(self.numerator)
+		out += '( {} )'.format(self.denominator)
+		return out
